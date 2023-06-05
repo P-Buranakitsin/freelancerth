ffade4ad92089a54796d445ac709ecb36f836dfa
Pavaruth Buranakitsin <2646802b@student.gla.ac.uk>
2023-06-05 13:08:18 +0100

    Added sass and eslint-webpack-plugin

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require("path");
+const ESLintPlugin = require('eslint-webpack-plugin');
 
-module.exports = nextConfig
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  webpack: (config, { dev }) => {
+    if (dev) {
+      // Add this plugin only in dev mode
+      config.plugins.push(
+        new ESLintPlugin({
+          context: "./", // Location where it will scan all the files
+          extensions: ["js", "jsx", "ts", "tsx"], // File formats that should be scanned
+          exclude: ["node_modules", "dist"], // Exclude everything in these folders
+        })
+      );
+    }
+    return config;
+  },
+};
+
+module.exports = nextConfig;
