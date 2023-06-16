693f542706d15770e434bf889370e38a895ad973
Pavaruth Buranakitsin <2646802b@student.gla.ac.uk>
2023-06-16 20:29:11 +0100

    Added remove image button in new-user page

--- src/app/auth/new-user/page.tsx
+++ src/app/auth/new-user/page.tsx
@@ -3,26 +3,48 @@
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import { BsPersonCircle } from "react-icons/bs";
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useRef, useState } from "react";
+import { useSession } from "next-auth/react";
+import { ImBin } from "react-icons/im";
 
 export default function VerifyRequest() {
   const router = useRouter();
-  const [image, setImage] = useState<File | null>(null);
+  const [image, setImage] = useState<File | string | null>(null);
+  const { data: session, status } = useSession();
+  const imageInputRef = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    console.log('ffgdg')
+    if (session?.user?.image) {
+      setImage(session.user.image);
+    }
+  }, [session?.user?.image]);
 
   const skip = () => {
     router.push("/");
   };
 
   const selectImage = (event: FormEvent<HTMLInputElement>) => {
-    console.log("fgdfgdfg");
     if (
       event.currentTarget.files &&
       event.currentTarget.files.length > 0 &&
       event.currentTarget.files[0].type.includes("image")
     ) {
       setImage(event.currentTarget.files[0]);
+    } else if (session?.user?.image) {
+      setImage(session.user.image);
     } else {
       setImage(null);
+    }
+  };
+
+  const removeImage = (event: FormEvent<HTMLButtonElement>) => {
+    if (session?.user?.image) {
+      setImage(session.user.image);
+    } else {
+      setImage(null);
+    }
+    if (imageInputRef.current) {
+      imageInputRef.current.value = "";
     }
   };
 
@@ -38,12 +60,12 @@ export default function VerifyRequest() {
               Please fill in your information{" "}
             </p>
             {image ? (
-              <Image
+              <img
                 className="inline-block h-[5rem] w-[5rem] rounded-full ring-2 ring-white dark:ring-gray-800 mt-5"
-                src={URL.createObjectURL(image)}
-                alt="Image Description"
-                width={5}
-                height={5}
+                src={
+                  typeof image === "string" ? image : URL.createObjectURL(image)
+                }
+                alt="Profile pic"
               />
             ) : (
               <BsPersonCircle
@@ -57,25 +79,35 @@ export default function VerifyRequest() {
             {/* Form */}
             <form>
               <div className="grid gap-y-4">
-                <div>
+                <div className="">
                   <div className="block text-sm mb-2 dark:text-white">
                     Profile picture
                   </div>
-                  <label htmlFor="image" className="sr-only">
-                    Choose file
-                  </label>
-                  <input
-                    type="file"
-                    name="image"
-                    accept="image/*"
-                    id="image"
-                    onChange={selectImage}
-                    className="block w-full border border-gray-200 shadow-sm rounded-md text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400
+                  <div className="flex">
+                    <label htmlFor="image" className="sr-only">
+                      Choose file
+                    </label>
+                    <input
+                      type="file"
+                      name="image"
+                      ref={imageInputRef}
+                      accept="image/*"
+                      id="image"
+                      onChange={selectImage}
+                      className="block w-full border border-gray-200 shadow-sm rounded-l-md text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400
     file:bg-transparent file:border-0
     file:bg-gray-100 file:mr-4
     file:py-3 file:px-4
     dark:file:bg-gray-700 dark:file:text-gray-400"
-                  />
+                    />
+                    <button
+                      type="button"
+                      onClick={removeImage}
+                      className="inline-flex flex-shrink-0 justify-center items-center h-[2.875rem] w-[2.875rem] rounded-r-md border border-transparent font-semibold bg-red-500 text-white hover:bg-red-600 focus:z-10 focus:outline-none focus:ring-2 focus:ring-red-500 transition-all text-sm"
+                    >
+                      <ImBin width={16} height={16} />
+                    </button>
+                  </div>
                 </div>
                 <div>
                   <label
@@ -87,6 +119,7 @@ export default function VerifyRequest() {
                   <div className="relative">
                     <input
                       type="text"
+                      defaultValue={session?.user?.name?.split(" ")[0] || ""}
                       id="first_name"
                       name="first_name"
                       className="border py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400"
@@ -129,6 +162,7 @@ export default function VerifyRequest() {
                       type="text"
                       id="last_name"
                       name="last_name"
+                      defaultValue={session?.user?.name?.split(" ")[1] || ""}
                       className="border py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400"
                       required
                       aria-describedby="last_name-error"
