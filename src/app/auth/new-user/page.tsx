9dde9aa955ac9ed3f5e76bea31aee34b377a30bc
Pavaruth Buranakitsin <2646802b@student.gla.ac.uk>
2023-06-16 18:54:30 +0100

    Front end for new-user page done!

--- src/app/auth/new-user/page.tsx
+++ src/app/auth/new-user/page.tsx
@@ -1,214 +1,180 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import { BsPersonCircle } from "react-icons/bs";
+import { FormEvent, useState } from "react";
+
 export default function VerifyRequest() {
-    return (
-      <main className="w-full max-w-md mx-auto p-6">
-        <div className="mt-7 bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-gray-800 dark:border-gray-700">
-          <div className="p-4 sm:p-7">
-            <div className="text-center">
-              <h1 className="block text-2xl font-bold text-gray-800 dark:text-white">
-                Sign up
-              </h1>
-              <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
-                Don&apos;t have an account yet?{" "}
-                <a
-                  className="text-blue-600 decoration-2 hover:underline font-medium"
-                  href="../examples/html/signup.html"
-                >
-                  Sign up here
-                </a>
-              </p>
-            </div>
-            <div className="mt-5">
-              {/* Form */}
-              <form>
-                <div className="grid gap-y-4">
-                  {/* Form Group */}
-                  <div>
+  const router = useRouter();
+  const [image, setImage] = useState<File | null>(null);
+
+  const skip = () => {
+    router.push("/");
+  };
+
+  const selectImage = (event: FormEvent<HTMLInputElement>) => {
+    console.log("fgdfgdfg");
+    if (
+      event.currentTarget.files &&
+      event.currentTarget.files.length > 0 &&
+      event.currentTarget.files[0].type.includes("image")
+    ) {
+      setImage(event.currentTarget.files[0]);
+    } else {
+      setImage(null);
+    }
+  };
+
+  return (
+    <main className="w-full max-w-lg mx-auto p-6">
+      <div className="mt-7 bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-gray-800 dark:border-gray-700">
+        <div className="p-4 sm:p-7">
+          <div className="text-center">
+            <h1 className="block text-2xl font-bold text-gray-800 dark:text-white">
+              Welcome
+            </h1>
+            <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
+              Please fill in your information{" "}
+            </p>
+            {image ? (
+              <Image
+                className="inline-block h-[5rem] w-[5rem] rounded-full ring-2 ring-white dark:ring-gray-800 mt-5"
+                src={URL.createObjectURL(image)}
+                alt="Image Description"
+                width={5}
+                height={5}
+              />
+            ) : (
+              <BsPersonCircle
+                className="inline-block h-[5rem] w-[5rem] rounded-full ring-2 ring-white dark:ring-gray-800 mt-5"
+                color="white"
+              />
+            )}
+          </div>
+
+          <div className="mt-5">
+            {/* Form */}
+            <form>
+              <div className="grid gap-y-4">
+                <div>
+                  <div className="block text-sm mb-2 dark:text-white">
+                    Profile picture
+                  </div>
+                  <label htmlFor="image" className="sr-only">
+                    Choose file
+                  </label>
+                  <input
+                    type="file"
+                    name="image"
+                    accept="image/*"
+                    id="image"
+                    onChange={selectImage}
+                    className="block w-full border border-gray-200 shadow-sm rounded-md text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400
+    file:bg-transparent file:border-0
+    file:bg-gray-100 file:mr-4
+    file:py-3 file:px-4
+    dark:file:bg-gray-700 dark:file:text-gray-400"
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="first_name"
+                    className="block text-sm mb-2 dark:text-white"
+                  >
+                    First name
+                  </label>
+                  <div className="relative">
+                    <input
+                      type="text"
+                      id="first_name"
+                      name="first_name"
+                      className="border py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400"
+                      required
+                      aria-describedby="first_name-error"
+                    />
+                    <div className="hidden absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
+                      <svg
+                        className="h-5 w-5 text-red-500"
+                        width={16}
+                        height={16}
+                        fill="currentColor"
+                        viewBox="0 0 16 16"
+                        aria-hidden="true"
+                      >
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                      </svg>
+                    </div>
+                  </div>
+                  <p
+                    className="hidden text-xs text-red-600 mt-2"
+                    id="first_name-error"
+                  >
+                    Please include a valid first name
+                  </p>
+                </div>
+                {/* End Form Group */}
+                {/* Form Group */}
+                <div>
+                  <div className="flex justify-between items-center">
                     <label
-                      htmlFor="email"
+                      htmlFor="last_name"
                       className="block text-sm mb-2 dark:text-white"
                     >
-                      Email address
+                      Last name
                     </label>
-                    <div className="relative">
-                      <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        className="border py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400"
-                        required
-                        aria-describedby="email-error"
-                      />
-                      <div className="hidden absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
-                        <svg
-                          className="h-5 w-5 text-red-500"
-                          width={16}
-                          height={16}
-                          fill="currentColor"
-                          viewBox="0 0 16 16"
-                          aria-hidden="true"
-                        >
-                          <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
-                        </svg>
-                      </div>
-                    </div>
-                    <p
-                      className="hidden text-xs text-red-600 mt-2"
-                      id="email-error"
-                    >
-                      Please include a valid email address so we can get back to
-                      you
-                    </p>
                   </div>
-                  {/* End Form Group */}
-                  {/* Form Group */}
-                  <div>
-                    <div className="flex justify-between items-center">
-                      <label
-                        htmlFor="password"
-                        className="block text-sm mb-2 dark:text-white"
+                  <div className="relative">
+                    <input
+                      type="text"
+                      id="last_name"
+                      name="last_name"
+                      className="border py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400"
+                      required
+                      aria-describedby="last_name-error"
+                    />
+                    <div className="hidden absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
+                      <svg
+                        className="h-5 w-5 text-red-500"
+                        width={16}
+                        height={16}
+                        fill="currentColor"
+                        viewBox="0 0 16 16"
+                        aria-hidden="true"
                       >
-                        Password
-                      </label>
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                      </svg>
                     </div>
-                    <div className="relative">
-                      <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        className="border py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400"
-                        aria-describedby="password-error"
-                      />
-                      <div className="hidden absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
-                        <svg
-                          className="h-5 w-5 text-red-500"
-                          width={16}
-                          height={16}
-                          fill="currentColor"
-                          viewBox="0 0 16 16"
-                          aria-hidden="true"
-                        >
-                          <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
-                        </svg>
-                      </div>
-                    </div>
-                    <p
-                      className="hidden text-xs text-red-600 mt-2"
-                      id="password-error"
-                    >
-                      8+ characters required
-                    </p>
                   </div>
-                  {/* End Form Group */}
+                  <p
+                    className="hidden text-xs text-red-600 mt-2"
+                    id="last_name-error"
+                  >
+                    Please include a valid last name
+                  </p>
+                </div>
+                {/* End Form Group */}
+                <div className="grid sm:grid-cols-2 grid-cols-1 gap-4 mt-4">
+                  <button
+                    type="button"
+                    onClick={skip}
+                    className="w-full py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-gray-800 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800"
+                  >
+                    Skip
+                  </button>
                   <button
                     type="submit"
                     className="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800"
                   >
-                    Sign in
+                    Submit
                   </button>
                 </div>
-              </form>
-              {/* End Form */}
-              <div className="py-3 flex items-center text-xs text-gray-400 before:flex-[1_1_0%] before:border-t before:border-gray-200 before:mr-6 after:flex-[1_1_0%] after:border-t after:border-gray-200 after:ml-6 dark:text-gray-500 dark:before:border-gray-600 dark:after:border-gray-600">
-                Or continue with
               </div>
-              <div className="grid sm:grid-cols-2 grid-cols-1 gap-4">
-                <button
-                  type="button"
-                  className="w-full py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-gray-800 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800"
-                >
-                  <svg
-                    className="h-5 w-5 mr-2"
-                    xmlns="http://www.w3.org/2000/svg"
-                    xmlnsXlink="http://www.w3.org/1999/xlink"
-                    viewBox="-0.5 0 48 48"
-                    version="1.1"
-                  >
-                    <g
-                      id="Icons"
-                      stroke="none"
-                      stroke-width="1"
-                      fill="none"
-                      fill-rule="evenodd"
-                    >
-                      <g
-                        id="Color-"
-                        transform="translate(-401.000000, -860.000000)"
-                      >
-                        <g
-                          id="Google"
-                          transform="translate(401.000000, 860.000000)"
-                        >
-                          <path
-                            d="M9.82727273,24 C9.82727273,22.4757333 10.0804318,21.0144 10.5322727,19.6437333 L2.62345455,13.6042667 C1.08206818,16.7338667 0.213636364,20.2602667 0.213636364,24 C0.213636364,27.7365333 1.081,31.2608 2.62025,34.3882667 L10.5247955,28.3370667 C10.0772273,26.9728 9.82727273,25.5168 9.82727273,24"
-                            id="Fill-1"
-                            fill="#FBBC05"
-                          >
-                            {" "}
-                          </path>
-                          <path
-                            d="M23.7136364,10.1333333 C27.025,10.1333333 30.0159091,11.3066667 32.3659091,13.2266667 L39.2022727,6.4 C35.0363636,2.77333333 29.6954545,0.533333333 23.7136364,0.533333333 C14.4268636,0.533333333 6.44540909,5.84426667 2.62345455,13.6042667 L10.5322727,19.6437333 C12.3545909,14.112 17.5491591,10.1333333 23.7136364,10.1333333"
-                            id="Fill-2"
-                            fill="#EB4335"
-                          >
-                            {" "}
-                          </path>
-                          <path
-                            d="M23.7136364,37.8666667 C17.5491591,37.8666667 12.3545909,33.888 10.5322727,28.3562667 L2.62345455,34.3946667 C6.44540909,42.1557333 14.4268636,47.4666667 23.7136364,47.4666667 C29.4455,47.4666667 34.9177955,45.4314667 39.0249545,41.6181333 L31.5177727,35.8144 C29.3995682,37.1488 26.7323182,37.8666667 23.7136364,37.8666667"
-                            id="Fill-3"
-                            fill="#34A853"
-                          >
-                            {" "}
-                          </path>
-                          <path
-                            d="M46.1454545,24 C46.1454545,22.6133333 45.9318182,21.12 45.6113636,19.7333333 L23.7136364,19.7333333 L23.7136364,28.8 L36.3181818,28.8 C35.6879545,31.8912 33.9724545,34.2677333 31.5177727,35.8144 L39.0249545,41.6181333 C43.3393409,37.6138667 46.1454545,31.6490667 46.1454545,24"
-                            id="Fill-4"
-                            fill="#4285F4"
-                          >
-                            {" "}
-                          </path>
-                        </g>
-                      </g>
-                    </g>
-                  </svg>
-                  Google
-                </button>
-                <button
-                  type="button"
-                  className="w-full py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-gray-800 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800"
-                >
-                  <svg
-                    className="h-5 w-5 mr-2"
-                    xmlns="http://www.w3.org/2000/svg"
-                    xmlnsXlink="http://www.w3.org/1999/xlink"
-                    viewBox="0 0 48 48"
-                    version="1.1"
-                  >
-                    <g
-                      id="Icons"
-                      stroke="none"
-                      stroke-width="1"
-                      fill="none"
-                      fill-rule="evenodd"
-                    >
-                      <g
-                        id="Color-"
-                        transform="translate(-200.000000, -160.000000)"
-                        fill="#4460A0"
-                      >
-                        <path
-                          d="M225.638355,208 L202.649232,208 C201.185673,208 200,206.813592 200,205.350603 L200,162.649211 C200,161.18585 201.185859,160 202.649232,160 L245.350955,160 C246.813955,160 248,161.18585 248,162.649211 L248,205.350603 C248,206.813778 246.813769,208 245.350955,208 L233.119305,208 L233.119305,189.411755 L239.358521,189.411755 L240.292755,182.167586 L233.119305,182.167586 L233.119305,177.542641 C233.119305,175.445287 233.701712,174.01601 236.70929,174.01601 L240.545311,174.014333 L240.545311,167.535091 C239.881886,167.446808 237.604784,167.24957 234.955552,167.24957 C229.424834,167.24957 225.638355,170.625526 225.638355,176.825209 L225.638355,182.167586 L219.383122,182.167586 L219.383122,189.411755 L225.638355,189.411755 L225.638355,208 L225.638355,208 Z"
-                          id="Facebook"
-                        ></path>
-                      </g>
-                    </g>
-                  </svg>
-                  Facebook
-                </button>
-              </div>
-            </div>
+            </form>
+            {/* End Form */}
           </div>
         </div>
-      </main>
-    );
-  }
-  
+      </div>
+    </main>
+  );
+}
