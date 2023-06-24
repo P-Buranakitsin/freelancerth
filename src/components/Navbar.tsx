873d31d3e4f98fb2c2349b6937ded2a1018096bb
Pavaruth Buranakitsin <2646802b@student.gla.ac.uk>
2023-06-24 18:56:15 +0100

    Redesigned navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,10 +17,7 @@ export default function Navbar() {
   };
 
   return (
-    <header
-      className="sticky top-0 inset-x-0 flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white border-b border-gray-200 text-sm py-3 sm:py-0 dark:bg-gray-800 dark:border-gray-700"
-      data-session-status={status}
-    >
+    <header className="sticky top-0 inset-x-0 flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white border-b border-gray-200 text-sm py-3 sm:py-0 dark:bg-gray-800 dark:border-gray-700">
       <nav
         className="relative max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8"
         aria-label="Global"
@@ -67,9 +64,9 @@ export default function Navbar() {
         </div>
         <div
           id="navbar-collapse-with-animation"
-          className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:block"
+          className="pl-2 hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:block"
         >
-          <div className="flex flex-col gap-y-4 gap-x-0 mt-5 sm:flex-row sm:items-center sm:justify-end sm:gap-y-0 sm:gap-x-7 sm:mt-0 sm:pl-7">
+          <div className="flex flex-col gap-y-4 gap-x-0 mt-5 sm:flex-row sm:items-center sm:gap-y-0 sm:gap-x-7 sm:mt-0 sm:pl-7">
             <a
               className="font-medium text-blue-600 sm:py-6 dark:text-blue-500"
               href="#"
@@ -78,186 +75,133 @@ export default function Navbar() {
               Landing
             </a>
             <a
-              className="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500"
+              className="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-white dark:hover:text-gray-300"
               href="#"
             >
               Account
             </a>
             <a
-              className="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500"
+              className="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-white dark:hover:text-gray-300"
               href="#"
             >
               Work
             </a>
             <a
-              className="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500"
+              className="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-white dark:hover:text-gray-300"
               href="#"
             >
               Blog
             </a>
-            <div className="hs-dropdown [--strategy:static] sm:[--strategy:fixed] [--adaptive:none] sm:[--trigger:hover] sm:py-4">
-              <button
-                type="button"
-                className="flex items-center w-full text-gray-500 hover:text-gray-400 font-medium dark:text-gray-400 dark:hover:text-gray-500 "
-              >
-                Dropdown
-                <svg
-                  className="ml-2 w-2.5 h-2.5 text-gray-600"
-                  width={16}
-                  height={16}
-                  viewBox="0 0 16 16"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                    strokeLinecap="round"
-                  />
-                </svg>
-              </button>
-              <div className="hs-dropdown-menu transition-[opacity,margin] duration-[0.1ms] sm:duration-[150ms] hs-dropdown-open:opacity-100 opacity-0 sm:w-48 hidden z-10 bg-white sm:shadow-md rounded-lg p-2 dark:bg-gray-800 sm:dark:border dark:border-gray-700 dark:divide-gray-700 before:absolute top-full sm:border before:-top-5 before:left-0 before:w-full before:h-5">
-                <a
-                  className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                  href="#"
-                >
-                  About
-                </a>
-                <div className="hs-dropdown relative [--strategy:static] sm:[--strategy:absolute] [--adaptive:none] sm:[--trigger:hover]">
-                  <button
-                    type="button"
-                    className="w-full flex justify-between w-full items-center text-sm text-gray-800 rounded-md py-2 px-3 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
+
+            <div
+              className={`flex items-center gap-x-2 sm:ml-auto pr-2 ${
+                status === "authenticated"
+                  ? "-sm:border-t-[1px] -sm:border-white"
+                  : ""
+              } `}
+            >
+              {status !== "authenticated" ? (
+                <button className="p-2 border-white border-2 cursor-pointer rounded-md hover:bg-gray-600 transition ease-in-out duration-500">
+                  <div
+                    className="flex items-center gap-x-2 font-medium text-gray-500  dark:text-white "
+                    onClick={handleSigninOnClick}
                   >
-                    Sub Menu
                     <svg
-                      className="sm:-rotate-90 ml-2 w-2.5 h-2.5 text-gray-600"
+                      className="w-4 h-4 text-white"
+                      xmlns="http://www.w3.org/2000/svg"
                       width={16}
                       height={16}
+                      fill="currentColor"
                       viewBox="0 0 16 16"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
                     >
-                      <path
-                        d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5"
-                        stroke="currentColor"
-                        strokeWidth={2}
-                        strokeLinecap="round"
-                      />
+                      <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
                     </svg>
-                  </button>
-                  <div className="hs-dropdown-menu transition-[opacity,margin] duration-[0.1ms] sm:duration-[150ms] hs-dropdown-open:opacity-100 opacity-0 sm:w-48 hidden z-10 sm:mt-2 bg-white sm:shadow-md rounded-lg p-2 dark:bg-gray-800 sm:dark:border dark:border-gray-700 dark:divide-gray-700 before:absolute sm:border before:-right-5 before:top-0 before:h-full before:w-5 top-0 right-full !mx-[10px]">
-                    <a
-                      className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                      href="#"
-                    >
-                      About
-                    </a>
-                    <a
-                      className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                      href="#"
-                    >
-                      Downloads
-                    </a>
-                    <a
-                      className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                      href="#"
-                    >
-                      Team Account
-                    </a>
+                    Log in
                   </div>
-                </div>
-                <a
-                  className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                  href="#"
-                >
-                  Downloads
-                </a>
-                <a
-                  className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                  href="#"
-                >
-                  Team Account
-                </a>
-              </div>
-            </div>
-            {status !== "authenticated" ? (
-              <button
-                className="flex items-center gap-x-2 font-medium text-gray-500 hover:text-blue-600 sm:border-l sm:border-gray-300 sm:my-6 sm:pl-6 dark:border-gray-700 dark:text-gray-400 dark:hover:text-blue-500"
-                onClick={handleSigninOnClick}
-              >
-                <svg
-                  className="w-4 h-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width={16}
-                  height={16}
-                  fill="currentColor"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
-                </svg>
-                <div>Log in</div>
-              </button>
-            ) : (
-              <div
-                className="hs-dropdown relative inline-flex sm:border-l sm:border-t-0 border-t sm:px-6 px-2 sm:py-0 py-4 sm:border-gray-300 items-center cursor-pointer"
-                data-hs-dropdown-placement="bottom-right"
-                id="open-profile"
-              >
-                <button
-                  id="hs-dropdown-with-header"
-                  type="button"
-                  className="inline-flex flex-shrink-0 justify-center items-center gap-2 h-[2.375rem] w-[2.375rem] rounded-full font-medium hover:bg-white/[.2] text-white align-middle focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-gray-900 transition-all text-xs"
-                >
-                  {session.user.image ? (
-                    <Image
-                      className="inline-block h-[2.375rem] w-[2.375rem] rounded-full"
-                      src={session.user.image || ""}
-                      width={200}
-                      alt="pic"
-                      height={200}
-                    />
-                  ) : (
-                    <BsPersonCircle
-                      className="inline-block h-[2.375rem] w-[2.375rem] rounded-full"
-                      color="white"
-                    />
-                  )}
                 </button>
-                <div className="sm:hidden font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500 ml-2">
-                  Profile
-                </div>
+              ) : (
                 <div
-                  className="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-[15rem] z-10 bg-white shadow-md rounded-lg p-2 dark:bg-gray-800 dark:border dark:border-gray-700"
-                  aria-labelledby="hs-dropdown-with-header"
+                  className="hs-dropdown relative inline-flex sm:pt-0 pt-4 items-center sm:cursor-pointer"
+                  data-hs-dropdown-placement="bottom-right"
+                  id="open-profile"
                 >
-                  <div className="py-3 px-5 -m-2 bg-gray-100 rounded-t-lg dark:bg-gray-700">
-                    <p className="text-sm text-gray-500 dark:text-gray-400">
+                  <button
+                    id="hs-dropdown-with-header"
+                    type="button"
+                    className="-sm:cursor-default inline-flex flex-shrink-0 justify-center items-center gap-2 h-[2.375rem] w-[2.375rem] rounded-full font-medium sm:hover:bg-white/[.2] text-white align-middle focus:outline-none sm:focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-gray-900 transition-all text-xs"
+                  >
+                    {session.user.image ? (
+                      <Image
+                        className="inline-block h-[2.375rem] w-[2.375rem] rounded-full"
+                        src={session.user.image || ""}
+                        width={200}
+                        alt="pic"
+                        height={200}
+                      />
+                    ) : (
+                      <BsPersonCircle
+                        className="inline-block h-[2.375rem] w-[2.375rem] rounded-full"
+                        color="white"
+                      />
+                    )}
+                  </button>
+                  <div className="flex flex-col">
+                    <p className="sm:hidden font-medium text-gray-500 sm:hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:sm:hover:text-gray-300 ml-2">
                       Signed in as
                     </p>
-                    <p className="text-sm font-medium text-gray-800 dark:text-gray-300">
+                    <p className="sm:hidden font-medium text-gray-500 sm:hover:text-gray-400 sm:py-6 dark:text-white dark:sm:hover:text-gray-300 ml-2">
                       {session.user.email}
                     </p>
                   </div>
-                  <div className="mt-2 py-2 first:pt-0 last:pb-0">
-                    <a
-                      className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                      href="#"
-                    >
-                      <ImProfile size={16} className="flex-none" />
-                      <div>My Profile</div>
-                    </a>
-                    <a
-                      className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                      onClick={handleSignoutOnClick}
-                      id="signout"
-                    >
-                      <BiLogOut size={16} className="flex-none" />
-                      <div>Log Out</div>
-                    </a>
+
+                  <div
+                    className="-sm:hidden hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-[15rem] z-10 bg-white shadow-md rounded-lg p-2 dark:bg-gray-800 dark:border dark:border-gray-700"
+                    aria-labelledby="hs-dropdown-with-header"
+                  >
+                    <div className="py-3 px-5 -m-2 bg-gray-100 rounded-t-lg dark:bg-gray-700">
+                      <p className="text-sm text-gray-500 dark:text-gray-400">
+                        Signed in as
+                      </p>
+                      <p className="text-sm font-medium text-gray-800 dark:text-gray-300">
+                        {session.user.email}
+                      </p>
+                    </div>
+                    <div className="mt-2 py-2 first:pt-0 last:pb-0">
+                      <a
+                        className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
+                        href="#"
+                      >
+                        <ImProfile size={16} className="flex-none" />
+                        <div>My Profile</div>
+                      </a>
+                      <a
+                        className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
+                        onClick={handleSignoutOnClick}
+                        id="signout"
+                      >
+                        <BiLogOut size={16} className="flex-none" />
+                        <div>Log Out</div>
+                      </a>
+                    </div>
                   </div>
                 </div>
-              </div>
+              )}
+            </div>
+            {status === "authenticated" && (
+              <>
+                <a
+                  className="sm:hidden font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-white dark:hover:text-gray-300"
+                  href="#"
+                >
+                  My Profile
+                </a>
+                <button
+                  className="sm:hidden mb-2 flex font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-white dark:hover:text-gray-300"
+                  onClick={handleSignoutOnClick}
+                >
+                  Sign out
+                </button>
+              </>
             )}
           </div>
         </div>
